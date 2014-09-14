7a830763d84cfb08ebe1c2452992252fee989e68
VanHook, Corwin <Corwin.VanHook@disney.com>
2014-09-14 12:56:26 -0700

    Any creepers within a 32x32x32 bounding box around the Creeper Totem block get set on fire!

--- src/main/java/com/corwinjv/mobtotems/Reference.java
+++ src/main/java/com/corwinjv/mobtotems/Reference.java
@@ -1,5 +1,11 @@
 package com.corwinjv.mobtotems;
 
+import cpw.mods.fml.common.FMLLog;
+import org.apache.logging.log4j.Level;
+import net.minecraft.entity.EntityList;
+
+import java.util.Iterator;
+
 /**
  * Created by vanhc011 on 8/31/14.
  */
@@ -14,4 +20,15 @@ public class Reference {
     public static final String SERVER_PROXY = "com.corwinjv.mobtotems.proxy.ServerProxy";
 
     public static final String GUI_FACTORY_CLASS = "com.corwinjv.mobtotems.gui.GuiFactoryMT";
+
+    public static void PrintEntityList()
+    {
+        Iterator entityNameItr = EntityList.stringToClassMapping.keySet().iterator();
+        FMLLog.log(Reference.MOD_NAME, Level.INFO, "-Printing Registered Entity Classes-");
+        while(entityNameItr.hasNext())
+        {
+            String key = (String)entityNameItr.next();
+            FMLLog.log(Reference.MOD_NAME, Level.INFO, String.format("     %s", key));
+        }
+    }
 }
--- src/main/java/com/corwinjv/mobtotems/blocks/BlockCreeperTotem.java
+++ src/main/java/com/corwinjv/mobtotems/blocks/BlockCreeperTotem.java
@@ -1,5 +1,24 @@
 package com.corwinjv.mobtotems.blocks;
 
+import com.corwinjv.mobtotems.Reference;
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.FMLLog;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.TickEvent;
+import cpw.mods.fml.common.registry.EntityRegistry;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.EntityList;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
+import org.apache.logging.log4j.Level;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
 /**
  * Created by vanhc011 on 9/1/14.
  */
@@ -9,7 +28,36 @@ public class BlockCreeperTotem extends BlockMT
     {
         super();
         setBlockName("creeperTotem");
+        setTickRandomly(true);
+
+        //World clientSideWorld = Minecraft.getMinecraft().theWorld;
     }
 
+    @Override
+    public void updateTick(World world, int x, int y, int z, Random rand)
+    {
+        super.updateTick(world, x, y, z, rand);
+        //FMLLog.log(Reference.MOD_NAME, Level.INFO, String.format("updateTick on block at %d, %d, %d", x, y, z));
 
+        // Find all mobs within bounding box that match the type creeper...
+        // Set those mobs on fire.
+        Class creeperClass = (Class) EntityList.stringToClassMapping.get("Creeper");
+        List entitiesWithinAABB = world.getEntitiesWithinAABB(creeperClass, AxisAlignedBB.getBoundingBox(x - 16, y - 16, z - 16, x + 16, y + 16, z + 16));
+
+        //FMLLog.log(Reference.MOD_NAME, Level.INFO, String.format("Entities found: %d", entitiesWithinAABB.size()));
+
+        for(int i = 0; i < entitiesWithinAABB.size(); i++)
+        {
+            EntityLivingBase creeperToBurn = (EntityLivingBase)entitiesWithinAABB.get(i);
+            creeperToBurn.setFire(3);
+        }
+
+        world.scheduleBlockUpdate(x, y, z, this, tickRate(world));
+    }
+
+    @Override
+    public int tickRate(World world)
+    {
+        return 10;
+    }
 }
