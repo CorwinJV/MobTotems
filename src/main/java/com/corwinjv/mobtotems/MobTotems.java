eae7b913bea851b0670c4b960615707c16df7fff
CorwinJV <corwin.j@gmail.com>
2016-01-23 14:25:40 -0800

    Re-add mobtotems as MobTotems.java

---
+++ src/main/java/com/corwinjv/mobtotems/MobTotems.java
@@ -0,0 +1,50 @@
+package com.corwinjv.mobtotems;
+/**
+ * Created by corwinjv on 8/30/14.
+ */
+
+import com.corwinjv.mobtotems.blocks.ModBlocks;
+import com.corwinjv.mobtotems.proxy.CommonProxy;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+import com.corwinjv.mobtotems.config.ConfigurationHandler;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+
+@Mod(modid=Reference.MOD_ID, name=Reference.MOD_NAME, version=Reference.MOD_VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
+public class MobTotems
+{
+    @Mod.Instance
+    public static MobTotems instance;
+
+    private static SimpleNetworkWrapper NETWORK = NetworkRegistry.INSTANCE.newSimpleChannel(Reference.MOD_ID);
+
+    @SidedProxy(clientSide = Reference.CLIENT_SIDE_PROXY_CLASS, serverSide = Reference.SERVER_SIDE_PROXY_CLASS)
+    public static CommonProxy proxy;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        ConfigurationHandler.Init(event.getSuggestedConfigurationFile());
+        MinecraftForge.EVENT_BUS.register(new ConfigurationHandler());
+
+        ModBlocks.init();
+        ModBlocks.registerBlocks();
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        proxy.registerRenders();
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+    }
+}
