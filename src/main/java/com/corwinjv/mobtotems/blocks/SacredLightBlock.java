f09ee63a8a9d5bcf8ea86a36235509fc9f423591
CorwinJV <corwin.j@gmail.com>
2016-07-15 01:49:43 -0700

    SacredLightBlock gets a light value and particle effects.

--- src/main/java/com/corwinjv/mobtotems/blocks/SacredLightBlock.java
+++ src/main/java/com/corwinjv/mobtotems/blocks/SacredLightBlock.java
@@ -5,10 +5,10 @@ import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
@@ -20,6 +20,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.Random;
 
 /**
  * Created by CorwinJV on 7/13/2016.
@@ -48,6 +49,23 @@ public class SacredLightBlock extends ModBlock implements ITileEntityProvider
     public BlockRenderLayer getBlockLayer()
     {
         return BlockRenderLayer.CUTOUT;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand)
+    {
+        double d0 = (double)pos.getX() + 0.5D;
+        double d1 = (double)pos.getY() + 0.7D;
+        double d2 = (double)pos.getZ() + 0.5D;
+
+        worldIn.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, d0 + 0.27D * (double)EnumFacing.UP.getFrontOffsetX(), d1 + 0.22D, d2 + 0.27D * (double)EnumFacing.UP.getFrontOffsetZ(), 0.0D, 0.0D, 0.0D);
+        worldIn.spawnParticle(EnumParticleTypes.FLAME, d0, d1, d2, 0.0D, 0.0D, 0.0D);
+    }
+
+    @Override
+    public int getLightValue(@Nullable IBlockState state, @Nullable IBlockAccess world, @Nullable BlockPos pos)
+    {
+        return 15;
     }
 
     // Can only place on the tops of solid things
