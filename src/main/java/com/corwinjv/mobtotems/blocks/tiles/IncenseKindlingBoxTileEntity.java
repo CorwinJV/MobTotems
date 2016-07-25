7b5b162ce869fc1f4c61ea4433d146cf5bd0a0cf
CorwinJV <corwin.j@gmail.com>
2016-07-24 17:55:23 -0700

    Add particle effects to show AOE of incense kindling
    Cloudy with a chance of villager

--- src/main/java/com/corwinjv/mobtotems/blocks/tiles/IncenseKindlingBoxTileEntity.java
+++ src/main/java/com/corwinjv/mobtotems/blocks/tiles/IncenseKindlingBoxTileEntity.java
@@ -3,15 +3,21 @@ package com.corwinjv.mobtotems.blocks.tiles;
 import baubles.api.BaublesApi;
 import com.corwinjv.mobtotems.interfaces.ICharged;
 import com.google.common.base.Predicate;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockAir;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.BlockPos;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by CorwinJV on 7/24/2016.
@@ -21,6 +27,7 @@ public class IncenseKindlingBoxTileEntity extends ModTileEntity
     private static final String CREATION_TIME = "CREATION_TIME";
 
     private static final long UPDATE_TICKS = 20;
+    private static final long PARTICLE_UPDATE_TICKS = 40;
     public static final double TMP_MANA_GAIN_DIST = 8;
     public static final int CHARGE_GAIN_PER_TICK = 2;
     private static final long TTL = 200;
@@ -66,7 +73,7 @@ public class IncenseKindlingBoxTileEntity extends ModTileEntity
         {
             performTTLUpdate();
 
-            long worldTime = getWorld().getWorldInfo().getWorldTime();
+            long worldTime = getWorld().getWorldTime();
             if(worldTime % UPDATE_TICKS == 0)
             {
                 performChargeAura();
@@ -74,7 +81,11 @@ public class IncenseKindlingBoxTileEntity extends ModTileEntity
         }
         else
         {
-            spawnParticleEffects();
+            long worldTime = getWorld().getWorldTime();
+            if(worldTime % PARTICLE_UPDATE_TICKS == 0)
+            {
+                spawnParticleEffects();
+            }
         }
     }
 
@@ -116,6 +127,57 @@ public class IncenseKindlingBoxTileEntity extends ModTileEntity
 
     private void spawnParticleEffects()
     {
+        double startX = pos.getX() - TMP_MANA_GAIN_DIST;
+        double startY = pos.getY() - TMP_MANA_GAIN_DIST;
+        double startZ = pos.getZ() - TMP_MANA_GAIN_DIST;
 
+        for(double x = startX; x < pos.getX() + TMP_MANA_GAIN_DIST; x++)
+        {
+            for(double y = startY; y < pos.getY() + TMP_MANA_GAIN_DIST; y++)
+            {
+                for(double z = startZ; z < pos.getZ() + TMP_MANA_GAIN_DIST; z++)
+                {
+                    BlockPos blockPos = new BlockPos(x, y, z);
+                    Block block = getWorld().getBlockState(blockPos).getBlock();
+                    Block blockAbove = getWorld().getBlockState(new BlockPos(x, y+1, z)).getBlock();
+
+                    if(pos.getDistance(blockPos.getX(), blockPos.getY(), blockPos.getZ()) < TMP_MANA_GAIN_DIST
+                            && block.isBlockSolid(getWorld(), blockPos, EnumFacing.UP)
+                            && blockAbove instanceof BlockAir)
+                    {
+                        Random rand = getWorld().rand;
+                        float width = 0.75f;
+                        float height = 0.75f;
+
+                        double motionX = rand.nextGaussian() * 0.02D;
+                        double motionY = rand.nextGaussian() * 0.02D;
+                        double motionZ = rand.nextGaussian() * 0.02D;
+                        worldObj.spawnParticle(
+                                EnumParticleTypes.CLOUD,
+                                x + rand.nextFloat() * width * 2.0F - width,
+                                y + 1.0D + rand.nextFloat() * height,
+                                z + rand.nextFloat() * width * 2.0F - width,
+                                motionX,
+                                motionY,
+                                motionZ);
+
+                        width = 1.0f;
+                        height = 1.0f;
+
+                        motionX = rand.nextGaussian() * 0.02D;
+                        motionY = rand.nextGaussian() * 0.02D;
+                        motionZ = rand.nextGaussian() * 0.02D;
+                        worldObj.spawnParticle(
+                                EnumParticleTypes.VILLAGER_HAPPY,
+                                x + rand.nextFloat() * width * 2.0F - width,
+                                y + 0.5D + rand.nextFloat() * height,
+                                z + rand.nextFloat() * width * 2.0F - width,
+                                motionX,
+                                motionY,
+                                motionZ);
+                    }
+                }
+            }
+        }
     }
 }
