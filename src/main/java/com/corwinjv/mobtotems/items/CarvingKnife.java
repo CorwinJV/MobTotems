6de5b6d1a40c56e1b37aefb632871a4e51652d1b
CorwinJV <corwin.j@gmail.com>
2017-01-18 14:43:59 -0800

    Update to latest forge
    Add CarvingKnife item

---
+++ src/main/java/com/corwinjv/mobtotems/items/CarvingKnife.java
@@ -0,0 +1,16 @@
+package com.corwinjv.mobtotems.items;
+
+/**
+ * Created by CorwinJV on 1/14/2017.
+ */
+public class CarvingKnife extends ModItem {
+    public CarvingKnife()
+    {
+        super();
+    }
+
+    @Override
+    public void init() {
+        super.init();
+    }
+}
