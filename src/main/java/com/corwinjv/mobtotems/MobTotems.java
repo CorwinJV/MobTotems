c3c147983b65debc7ba34c488e270740f11d27cd
CorwinJV <corwin.j@gmail.com>
2017-01-27 14:10:15 -0800

    Add cow totem cost & logic

--- src/main/java/com/corwinjv/mobtotems/MobTotems.java
+++ src/main/java/com/corwinjv/mobtotems/MobTotems.java
@@ -5,6 +5,7 @@ package com.corwinjv.mobtotems;
 
 import com.corwinjv.mobtotems.blocks.ModBlocks;
 import com.corwinjv.mobtotems.blocks.SacredLightBlock;
+import com.corwinjv.mobtotems.blocks.tiles.TotemLogic.CowLogic;
 import com.corwinjv.mobtotems.blocks.tiles.TotemLogic.CreeperLogic;
 import com.corwinjv.mobtotems.blocks.tiles.TotemLogic.EnderLogic;
 import com.corwinjv.mobtotems.entities.ModEntities;
@@ -46,6 +47,7 @@ public class MobTotems
         // Helper stuff
         TotemHelper.init();
         MinecraftForge.EVENT_BUS.register(new CreeperLogic.CreeperTotemEntityJoinWorldEvent());
+        MinecraftForge.EVENT_BUS.register(new CowLogic.CowTotemEntityJoinWorldEvent());
         MinecraftForge.EVENT_BUS.register(new EnderLogic.EnderTotemEnderTeleportEvent());
 
         // Blocks
--- src/main/java/com/corwinjv/mobtotems/blocks/tiles/TotemLogic/CowLogic.java
+++ src/main/java/com/corwinjv/mobtotems/blocks/tiles/TotemLogic/CowLogic.java
@@ -1,6 +1,18 @@
 package com.corwinjv.mobtotems.blocks.tiles.TotemLogic;
 
+import com.corwinjv.mobtotems.TotemHelper;
+import com.corwinjv.mobtotems.blocks.TotemType;
+import com.corwinjv.mobtotems.blocks.tiles.OfferingBoxTileEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.monster.IMob;
+import net.minecraft.entity.passive.EntityAnimal;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
@@ -14,6 +26,7 @@ public class CowLogic extends TotemLogic {
     public List<ItemStack> getCost() {
         {
             List<ItemStack> cost = new ArrayList<>();
+            cost.add(new ItemStack(Blocks.TALLGRASS, 4, 1));
             return cost;
         }
     }
@@ -23,4 +36,39 @@ public class CowLogic extends TotemLogic {
     public EffectType getEffectType() {
         return EffectType.EFFECT;
     }
+
+    public static class CowTotemEntityJoinWorldEvent
+    {
+        @SubscribeEvent
+        public void onEntityJoinWorldEvent(EntityJoinWorldEvent e)
+        {
+            if(!e.getWorld().isRemote)
+            {
+                List<TileEntity> loadedTileEntityList = e.getWorld().loadedTileEntityList;
+
+                for (TileEntity tileEntity : loadedTileEntityList) {
+                    if(tileEntity instanceof OfferingBoxTileEntity) {
+                        if(((OfferingBoxTileEntity)tileEntity).getChargeLevel() > 0) {
+                            if(TotemHelper.hasTotemType(e.getWorld(), (OfferingBoxTileEntity)tileEntity, TotemType.COW)) {
+                                Modifiers mods = TotemHelper.getModifiers(e.getWorld(), (OfferingBoxTileEntity)tileEntity);
+                                int radius = TotemHelper.DEFAULT_RADIUS + (int)(TotemHelper.RANGE_BOOST * mods.range);
+
+                                if(!canSpawnMobHere(tileEntity.getPos(), e.getEntity(), radius)) {
+                                    //FMLLog.log(Level.ERROR, "Stopped mob from spawning with radius: " + radius);
+                                    e.setCanceled(true);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private boolean canSpawnMobHere(BlockPos pos, Entity mob, int radius) {
+            double dist = mob.getPosition().getDistance(pos.getX(), pos.getY(), pos.getZ());
+            return !(mob instanceof EntityAnimal && dist < radius);
+        }
+    }
+
 }
