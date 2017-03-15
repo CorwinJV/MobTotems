2d2314b6a2b4fc466ff6509eaee357ea510c4ece
CorwinJV <corwin.j@gmail.com>
2017-03-14 17:27:48 -0700

    Add unit test
    testing ci

---
+++ src/test/java/com/corwinjv/mobtotems/blocks/tiles/TotemLogic/CowLogicTest.java
@@ -0,0 +1,33 @@
+package com.corwinjv.mobtotems.blocks.tiles.TotemLogic;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by CorwinJV on 3/14/2017.
+ */
+public class CowLogicTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getCost() throws Exception {
+
+    }
+
+    @Test
+    public void getEffectType() throws Exception {
+
+    }
+
+}
